ecda5bbafd1d11107f7247e9a29ee3af70d5546c
medved577 <andrey.medved577@yandex.ru>
2021-07-15 23:12:28 +0300

    Домашнее задание к первому уроку

---
+++ Medvedev_Andrey_dz_1/task_2_b.py
@@ -0,0 +1,18 @@
+my_list = [n ** 3 for n in range(1000)][1::2]
+sum_value = 0 # общая сумма чисел
+
+# b. Увеличим на 17 и вычислим сумму тех чисел из этого списка, сумма цифр которых делится нацело на 7
+for idx in range(len(my_list)):
+    value_list = my_list[idx] + 17
+    print(value_list)
+    sum_number = 0  # сумма числа
+
+    while value_list > 0:
+        sum_number += value_list % 10
+        value_list = value_list // 10
+
+    if sum_number % 7 == 0 :
+        sum_value = sum_value + sum_number
+
+
+print(sum_value)
